d9fd714cd86f6dacfa9fe0dc7d70871d16c7d21e
cangjian <m15927374217@163.com>
2020-03-19 22:00:21 +0800

    first commit

---
+++ src/main/java/com/cangjian/datastructure_and_algorithm/DatastructureAndAlgorithmApplication.java
@@ -0,0 +1,13 @@
+package com.cangjian.datastructure_and_algorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatastructureAndAlgorithmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatastructureAndAlgorithmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cangjian/datastructure_and_algorithm/DatastructureAndAlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cangjian.datastructure_and_algorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatastructureAndAlgorithmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
